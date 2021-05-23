43ab42b37d61601b2196c41dca225f6799574a23
stegano <steganowork@naver.com>
2021-05-23 22:12:34 +0900

    'NFC_DISCOVERED'

--- app/src/main/java/com/stegano/closertag/MainActivity.kt
+++ app/src/main/java/com/stegano/closertag/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.stegano.closertag
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.net.Uri
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = "MainActivity"
+    lateinit var nfcPendingIntent: PendingIntent
+    lateinit var nfcAdapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        nfcPendingIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
+        )
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        if (intent?.action == NfcAdapter.ACTION_NDEF_DISCOVERED){
+            val messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            if(messages == null) return
+
+            for (i in messages.indices){
+                showMsg(messages[i] as NdefMessage)
+            }
+        }
+    }
+
+
+    fun showMsg(mMessage: NdefMessage) {
+        val recs = mMessage.records
+
+        for (i in recs.indices) {
+            val record = recs[i]
+            Log.e(TAG, "msg: ${record}")
+
+            if (Arrays.equals(record.type, NdefRecord.RTD_URI)) {
+                val u: Uri = record.toUri()
+                val j = Intent(Intent.ACTION_VIEW)
+                j.data = u
+                startActivity(j)
+                finish()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, null, null);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter.disableForegroundDispatch(this);
     }
 }
