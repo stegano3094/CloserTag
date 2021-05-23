77d063a941acb5e2df5687ce78bd012b43c03abc
stegano <steganowork@naver.com>
2021-05-24 00:07:51 +0900

    'NFC_Test'

--- app/src/main/java/com/stegano/closertag/MainActivity.kt
+++ app/src/main/java/com/stegano/closertag/MainActivity.kt
@@ -2,20 +2,26 @@ package com.stegano.closertag
 
 import android.app.PendingIntent
 import android.content.Intent
-import android.net.Uri
+import android.content.IntentFilter
 import android.nfc.NdefMessage
 import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
 import android.nfc.Tag
+import android.nfc.tech.MifareUltralight
+import android.nfc.tech.Ndef
+import android.nfc.tech.NfcF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import java.util.*
+import android.widget.Toast
+import java.lang.reflect.Array
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
-    lateinit var nfcPendingIntent: PendingIntent
-    lateinit var nfcAdapter: NfcAdapter
+
+    private lateinit var nfcPendingIntent: PendingIntent
+    private lateinit var nfcAdapter: NfcAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,37 +34,6 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-
-        if (intent?.action == NfcAdapter.ACTION_NDEF_DISCOVERED){
-            val messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
-            if(messages == null) return
-
-            for (i in messages.indices){
-                showMsg(messages[i] as NdefMessage)
-            }
-        }
-    }
-
-
-    fun showMsg(mMessage: NdefMessage) {
-        val recs = mMessage.records
-
-        for (i in recs.indices) {
-            val record = recs[i]
-            Log.e(TAG, "msg: ${record}")
-
-            if (Arrays.equals(record.type, NdefRecord.RTD_URI)) {
-                val u: Uri = record.toUri()
-                val j = Intent(Intent.ACTION_VIEW)
-                j.data = u
-                startActivity(j)
-                finish()
-            }
-        }
-    }
-
     override fun onResume() {
         super.onResume()
         nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, null, null);
@@ -67,5 +42,38 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val detectedTag : Tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)!!;
+        val writeValue : String = "http://www.naver.com";
+        val message: NdefMessage = createTagMessage(writeValue);
+
+        writeTag(message, detectedTag);
+    }
+
+    private fun createTagMessage(msg: String): NdefMessage {
+        return NdefMessage(NdefRecord.createUri(msg))
+    }
+
+    private fun writeTag(message: NdefMessage, tag: Tag) {
+        val size = message.toByteArray().size
+        try {
+            val ndef = Ndef.get(tag)
+            if (ndef != null) {
+                ndef.connect()
+                if (!ndef.isWritable) {
+                    Toast.makeText(applicationContext, "can not write NFC tag", Toast.LENGTH_SHORT).show()
+                }
+                if (ndef.maxSize < size) {
+                    Toast.makeText(applicationContext, "NFC tag size too large", Toast.LENGTH_SHORT).show()
+                }
+                ndef.writeNdefMessage(message)
+                Toast.makeText(applicationContext, "NFC tag is writted", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, e.printStackTrace().toString());
+        }
     }
 }
